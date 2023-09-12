62b96259239fac2cf57bf6e4a8c3213db61d3b4e
jekwupeter <cephaspeter123@gmail.com>
2023-09-12 05:04:39 +0100

    added model

--- SimpleAPI-HNG-1/Controllers/ClientController.cs
+++ SimpleAPI-HNG-1/Controllers/ClientController.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace SimpleAPI_HNG_1.Controllers
 {
@@ -9,17 +12,25 @@ namespace SimpleAPI_HNG_1.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public async Task<string> Get(string slack_name, string track)
         {
-            
+            var user = new UserModel
+            {
+                slack_name = slack_name,
+                track = track
+            };
+
+            string json = JsonSerializer.Serialize(user);
+            return json;
         }
 
         public class UserModel 
         {
             public string slack_name { get; set; }
             public string current_day { get; set; } = DateTime.Now.Day.ToString();
-            public string utc_time { get; set; } = DateTime.UtcNow.ToString();
+            public string utc_time { get; set; } = DateTime.Now.ToString();
             public string track { get; set; } = "";
             public string github_file_url { get; set; } = "";
-            public string github_repo_url { get; set; } = "";
+            public string github_repo_url { get; set; } = "https://github.com/jekwupeter/SimpleAPI-HNG-1";
+            public int status_code { get; set; } = ((int)HttpStatusCode.OK);
         }
     }
 }
